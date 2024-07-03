205c14a8315646b8523e0a9f9cff63d2bcd5f963
TestingUser <maxamedcabdicasiis910@gmail.com>
2024-07-03 20:50:09 +0300

    done unclear

--- component/footerhtml.php
+++ component/footerhtml.php
@@ -15,7 +15,7 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
   <script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.html5.min.js"></script>
   <script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.print.min.js"></script>
-  <script src="../assets/js/own.js"></script>
+  <!-- <script src="../assets/js/own.js"></script> -->
 
   <!-- <script>
     function Nme(argument) {
